b2eaf94cd051502242aa93a37d15f3cb4708d287
David <david@webcodr.de>
2014-04-25 22:13:05 +0200

    Add user scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140425192656) do
+ActiveRecord::Schema.define(version: 20140425194626) do
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "victims", force: true do |t|
     t.date     "date"
