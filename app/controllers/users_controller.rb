fab69013c1bb423981ed872854d310a0c74559df
David <david@webcodr.de>
2014-05-03 02:36:34 +0200

    Allow create method to be called without login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_filter :authorize!, except: [:new]
+  before_filter :authorize!, except: [:new, :create]
 
   # GET /users
   # GET /users.json
