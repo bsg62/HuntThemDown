629da1f73ab7a60d98f29b601d4ce240dbd1eff0
David <david@webcodr.de>
2014-05-05 20:23:52 +0200

    Add development seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+OffenseType.create(id: 1, name: 'Warning')
+OffenseType.create(id: 2, name: 'Suspension')
+
+Victim.create(id: 1, name: 'The Troll')
+
+User.create(id: 1, name: 'Test', password: 'test')
+
+Offense.create(
+  id: 1,
+  victim_id: 1,
+  offense_type_id: 1,
+  user_id: 1,
+  name: 'Troll 1',
+  duration: '',
+  expires_at: nil,
+  reason: 'is a troll',
+  url: 'http://www.google.com/',
+  ip_address: '127.0.0.1',
+  hostname: 'localhost'
+)
+
+Comment.create(
+  id: 1,
+  user_id: 1,
+  victim_id: 1,
+  body: 'Beware! It\'s a troll!'
+)
