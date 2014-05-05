581d2c0b2e091602bbbce4c8d84a70443cdacb23
David <david@webcodr.de>
2014-05-05 23:11:16 +0200

    Use helper methods

--- app/helpers/victims_helper.rb
+++ app/helpers/victims_helper.rb
@@ -1,2 +1,14 @@
 module VictimsHelper
+  include Memoizable
+
+  def offenses
+    @victim.offenses
+  end
+  memoize :offenses
+
+  def comments
+    @victim.comments
+  end
+  memoize :comments
+
 end
