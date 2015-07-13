94114568839ff196b3349ff73ce6bb6ca0974351
David <david@webcodr.de>
2015-07-13 22:17:56 +0200

    Add more quotes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,6 +20,22 @@ module ApplicationHelper
     'Do a barrel roll!',
     'FINISH HIM!',
     'No more Mr. Nice Gaius!',
+    'As of this moment, we are at war!',
+    'Sometimes, you have to roll a hard six.',
+    'Prepare for unforseen consequences.',
+    'Do a barrel roll!',
+    'A man chooses; A slave obeys!',
+    'Nuclear launch detected.',
+    'Hard to see big picture behind pile of corpses.',
+    'Had to be me. Someone else might have gotten it wrong.',
+    'The best solution to a problem is usually the easiest one ...',
+    'Space. Space. I\'m in space. SPAAAAAAACE!',
+    'It ain\'t no secret I didn\'t get these scars falling over in church.',
+    'I\'m Commander Shepard, and this is my favorite store on the citadel.',
+    'Once an enemy, always an enemy.',
+    'Rage is a hell of an anesthetic.',
+    'Flammable! Or inflammable! Forget which! Doesn\'t matter!',
+    'You exist because we allow it and you will end because we demand it.'
   ].freeze
 
   def random_quote
