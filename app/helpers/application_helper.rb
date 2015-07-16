3381e01056acee497bf10257e689ef07766428bd
David <david@webcodr.de>
2015-07-16 22:38:16 +0200

    Add new quotes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,7 +35,16 @@ module ApplicationHelper
     'Once an enemy, always an enemy.',
     'Rage is a hell of an anesthetic.',
     'Flammable! Or inflammable! Forget which! Doesn\'t matter!',
-    'You exist because we allow it and you will end because we demand it.'
+    'You exist because we allow it and you will end because we demand it.',
+    'Then... DIE!',
+    'All right! This chick is TOAST!',
+    'No, we\'re exterminators. Someone saw a cockroach up on twelve.',
+    'We came, we saw, we kicked its ass!',
+    'Ah, if there\'s a steady paycheck in it, I\'ll believe anything you say.',
+    'That\'s a big Twinkie.',
+    'He didn\'t die of old age, either. He was poisoned, stabbed, shot, hung, stretched, disembowled, drawn and quartered.',
+    'Ray... we\'d like to shoot the monster. Could you move, please?',
+    'and have you BURNED AT THE STAKE!'
   ].freeze
 
   def random_quote
